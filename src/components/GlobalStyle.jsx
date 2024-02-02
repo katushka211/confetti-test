06db9e103d5c283233dbec276cfa336b97ec0cf8
Kateryna Verveda <katushka211@gmail.com>
2024-02-03 00:31:50 +0100

    add fonts and global style

---
+++ src/components/GlobalStyle.jsx
@@ -0,0 +1,65 @@
+import { createGlobalStyle } from "styled-components";
+import "modern-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*,
+*::after,
+*::before {
+  box-sizing: inherit;
+}
+
+body {
+  margin: 0;
+  font-family:
+    'Inter',
+    'Anzeigen Grotesk T',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-weight: 400;
+  line-height: 1.29;
+}
+
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+
+ul{
+  margin-left: 0;
+  padding: 0;
+}
+
+li {
+  list-style: none;
+  text-decoration: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+button {
+  padding: 0;
+  font-family: inherit;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+`;
