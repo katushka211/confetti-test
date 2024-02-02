99034bc08be2c8c2b2b8a81c2541fd13804dc199
Kateryna Verveda <katushka211@gmail.com>
2024-02-02 14:48:09 +0100

    add description

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logoIcon from "../images/svg/logo.svg";
+export const Header = () => {
+  return (
+    <header>
+      <img
+        src={logoIcon}
+        alt="logo"
+      />
+    </header>
+    //   <button>
+
+    //           Menu
+    //   </button>
+  );
+};
---
+++ src/components/Logo.jsx
@@ -0,0 +1,3 @@
+export const Logo = () => {
+  return <span>Confetti</span>;
+};
