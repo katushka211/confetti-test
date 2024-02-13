65edbb4b4de0777618d418c45965ad176b6c425b
Kateryna Verveda <katushka211@gmail.com>
2024-02-13 11:46:01 +0100

    add form validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,42 @@
+// import {
+//   Button,
+//   ContactForm,
+//   ContainerForm,
+//   Input,
+//   InputWrap,
+//   Label,
+//   TextArea,
+// } from './Form.styled';
+// // import { toast } from 'react-toastify';
+// // import 'react-toastify/dist/ReactToastify.css';
+
+// export const Form = () => {
+//   return (
+//     <ContainerForm>
+//       <ContactForm>
+//         <InputWrap>
+//           <Label htmlFor="name">Imię*</Label>
+//           <Input type="text" name="name" placeholder="Imię" />
+//         </InputWrap>
+//         <InputWrap>
+//           <Label htmlFor="email">E-mail*</Label>
+//           <Input type="email" name="email" placeholder="mail@gmail.com" />
+//         </InputWrap>
+//         <InputWrap>
+//           <Label htmlFor="message">Wiadomość</Label>
+//           <TextArea
+//             type="text"
+//             name="message"
+//             placeholder="Twoja wiadomość..."
+//           />
+//         </InputWrap>
+//         <Button type="submit">Wyślij</Button>
+//       </ContactForm>
+//     </ContainerForm>
+//   );
+// };
+
+import { useState } from 'react';
 import {
   Button,
   ContactForm,
@@ -7,31 +46,111 @@ import {
   Label,
   TextArea,
 } from './Form.styled';
-// import { toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Form = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [nameError, setNameError] = useState('');
+
+  const nameRegex = /^[\p{L}\s.'-]+$/u;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  const handleNameChange = event => {
+    const newName = event.target.value;
+    setName(newName);
+
+    if (!newName || !nameRegex.test(newName)) {
+      setNameError('Nieprawidłowe Іmię');
+    } else {
+      setNameError('');
+    }
+  };
+
+  const handleEmailChange = event => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+
+    if (!newEmail || !emailRegex.test(newEmail)) {
+      setEmailError('Nieprawidłowy email');
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const handleMessageChange = event => {
+    const newMessage = event.target.value;
+    setMessage(newMessage);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (!name || !email || nameError || emailError) {
+      setNameError('Imię wymagane');
+      setEmailError('E-mail wymagany');
+      return;
+    }
+
+    setName('');
+    setEmail('');
+    setMessage('');
+
+    toast.success('Zrobione', {
+      position: 'top-center',
+    });
+  };
+
   return (
     <ContainerForm>
-      <ContactForm>
-        <InputWrap>
-          <Label htmlFor="name">Imię*</Label>
-          <Input type="text" name="name" placeholder="Imię" />
+      <ContactForm onSubmit={handleSubmit}>
+        <InputWrap $hasError={!!nameError}>
+          <Label htmlFor="name" error={nameError}>
+            Imię*
+          </Label>
+          <Input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Imię"
+            // error={nameError}
+            $hasError={!!nameError}
+          />
+          {nameError && <span style={{ color: 'red' }}>{nameError}</span>}
         </InputWrap>
-        <InputWrap>
-          <Label htmlFor="email">E-mail*</Label>
-          <Input type="email" name="email" placeholder="mail@gmail.com" />
+        <InputWrap $hasError={!!emailError}>
+          <Label htmlFor="email" error={emailError}>
+            E-mail*
+          </Label>
+          <Input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="mail@gmail.com"
+            // error={emailError}
+            $hasError={!!emailError}
+          />
+          {emailError && <span style={{ color: 'red' }}>{emailError}</span>}
         </InputWrap>
         <InputWrap>
           <Label htmlFor="message">Wiadomość</Label>
           <TextArea
             type="text"
             name="message"
+            value={message}
+            onChange={handleMessageChange}
             placeholder="Twoja wiadomość..."
           />
         </InputWrap>
-        <Button type="submit">Wyślij</Button>
+        <Button type="submit" disabled={nameError || emailError}>
+          Wyślij
+        </Button>
       </ContactForm>
+      <ToastContainer position="top-center" />
     </ContainerForm>
   );
 };
